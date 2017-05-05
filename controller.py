3ed302c0c1c55a37e3bf34cda46ada41e3275d34
TimBray2 <timbrayy@gmail.com>
2017-05-05 13:34:12 +1200

    Version 2.4
    Extract the class “Display”, out of the Controller class and place in a new file

--- controller.py
+++ controller.py
@@ -9,6 +9,7 @@ from validate import CheckInput
 from save import Save
 from sys import argv
 from load import Load
+from display import Display
 
 
 class Controller:
@@ -26,6 +27,7 @@ class Controller:
         self.__pickle_data = PickleData()
         self.__save = Save(self.__db)
         self.__load_data = Load()
+        self.__display = Display(self.__db)
 
     def go(self, controller):
         self.__cmd_view.set_controller(controller)
@@ -51,32 +53,9 @@ class Controller:
         self.__load_location = self.__load_data.get_load_location()
 
     def display(self, format):
-        if format == "unchecked":
-            for row in self.__loaded_input:
-                print(row)
-            print("Unchecked data has been displayed")
-        elif format == "stored":
-            if not isinstance(self.__stored_data, str):
-                for row in range(len(self.__stored_data)):
-                    print(self.__stored_data[row])
-                print("Stored data has been displayed")
-            else:
-                print(self.__stored_data)
-        elif format == "graph":
-            if not isinstance(self.__stored_data, str):
-                graph_maker.create_graph(self.__stored_data)
-                print("Graph has been created in the browser.")
-            else:
-                print("Please validate and saved data before creating a graph.")
-        elif format == "database":
-            if not self.__database_flag:
-                self.__db.create_database()
-                self.__database_flag = True
-            for item in self.__db.load_database():
-                print(item)
-            print("Contents of database have been displayed.")
-        else:
-            print("Please select to display the data that is 'unchecked', 'stored', 'graph' or 'database'")
+        self.__display.display_data(format, self.__loaded_input,
+                                    self.__stored_data, self.__database_flag)
+        self.__database_flag = self.__display.get_database_flag()
 
     def validate(self, line):
         try:
